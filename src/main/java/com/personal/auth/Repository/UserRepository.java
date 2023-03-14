78af403817e2d13ea2cd62b613c7a364701cf28d
RcCat <r.caussignac@cat-amania.com>
2023-03-14 15:02:09 +0100

    init

---
+++ src/main/java/com/personal/auth/Repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.personal.auth.Repository;
+
+import com.personal.auth.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByName(String name);
+
+}
